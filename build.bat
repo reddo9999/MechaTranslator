7aff48febd2f4686c86433e4cc4dcbce78d8e720
Reddo <r@r.com>
2017-03-01 17:07:42 -0300

    - Use old python for compatibility - Zip Binary - Delete build folder

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-python setup.py build
+C:\Python33\python.exe setup.py build
+"C:\Program Files\7-Zip\7z.exe" a "MechaTranslator Binary.zip" -tzip "E:\Z\MechaTranslator\build\exe.win32-3.3\*" -mx=9
+rmdir build /s /q
