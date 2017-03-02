f220172709c6055fc499bd6274d24d95d04d1784
Reddo <r@r.com>
2017-03-02 11:27:48 -0300

    Remove redundancy, add comments, make 0Level translations get actually used

--- lib/classes/TranslationDictionary.py
+++ lib/classes/TranslationDictionary.py
@@ -1,7 +1,6 @@
 from .version import version
 
-contextNames = ["0Level", "RPGMakerV2", "InlineScript", "Vocabulary", "GameTitle", "Names", "Choice", "Dialogue", "Description", "System", "OtherText", "MV"]
-
+_CONTEXT_HIGH_PRIORITY = "0Level"
 _CONTEXT_INLINESCRIPT = "InlineScript"
 _CONTEXT_VOCABULARY = "Vocabulary"
 _CONTEXT_NAMES = "Names"
@@ -14,6 +13,9 @@ _CONTEXT_V2 = "RPGMakerV2"
 _CONTEXT_GAME_TITLE = "GameTitle"
 _CONTEXT_NOT_FOUND = "OtherText"
 
+contextNames = [_CONTEXT_HIGH_PRIORITY, _CONTEXT_NOT_FOUND, _CONTEXT_GAME_TITLE, _CONTEXT_V2, _CONTEXT_MV, _CONTEXT_SYSTEM,
+                _CONTEXT_DESCRIPTION, _CONTEXT_DIALOGUE, _CONTEXT_CHOICE, _CONTEXT_NAMES, _CONTEXT_VOCABULARY, _CONTEXT_INLINESCRIPT]
+
 class TranslationDictionary (object):
     def __init__(self, dictDict):
         self.dicts = dictDict
@@ -23,6 +25,7 @@ class TranslationDictionary (object):
         contextedDict[originalSentence] = translatedSentence + machineTranslatorTag()
 
     def hasTranslation (self, context, originalSentence):
+        # Checks context dictionary for originalSentence, returns boolean regarding it being found
         if originalSentence in self.dicts['0Level']:
             trans = self.dicts['0Level'][originalSentence]
             if not isMachineTranslation(trans) or isCurrentMachineTranslation(trans):
@@ -34,42 +37,25 @@ class TranslationDictionary (object):
         return False
 
     def getTranslation (self, context, originalSentence):
-        trans = self.dicts[context][originalSentence]
+        # Returns dictionary translation for originalSentence in context
+        if originalSentence in self.dicts['0Level']:
+            trans = self.dicts['0Level'][originalSentence]
+        else:
+            trans = self.dicts[context][originalSentence]
         if not isMachineTranslation(trans):
             return trans
         if isCurrentMachineTranslation(trans):
             index = trans.find(machineTranslatorTag())
             return trans[:index]
 
-    # def getBestContext (self, translationBlock):
-    #     if translationBlock.isV2():
-    #         return "RPGMakerV2"
-    #     if translationBlock.isMV():
-    #         return translationBlock.getCurrentContext()
-    #     if translationBlock.isDescription():
-    #         return "Description"
-    #     if translationBlock.isInlineScript():
-    #         return "InlineScript"
-    #     if translationBlock.isVocabulary():
-    #         return "Vocabulary"
-    #     if translationBlock.isGameTitle():
-    #         return "GameTitle"
-    #     if translationBlock.isNames():
-    #         return "Names"
-    #     if translationBlock.isSystem():
-    #         return "System"
-    #     if translationBlock.isChoice():
-    #         return "Choice"
-    #     if translationBlock.isDialogue():
-    #         return "Dialogue"
-    #     print ("Unrecognized context: " + str(translationBlock.contexts))
-    #     return "OtherText"
-
 def machineTranslatorTag ():
+    # Returns a full tag to be added to machine translated dictionary entries
     return "*MechaTranslator V" + version + "*"
 
 def isMachineTranslation (text):
+    # Returns boolean regarding text being an machine translated entry
     return text.find("*MechaTranslator V") != -1
 
 def isCurrentMachineTranslation (text):
+    # Returns boolean regarding text being an up to date machine translated entry
     return text.find(machineTranslatorTag()) != -1
